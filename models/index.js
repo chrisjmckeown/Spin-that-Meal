3d4e6dfb046cde2967784c64d1c6fc2162eb18fb
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-11-10 21:44:56 +1100

    sever working

--- models/index.js
+++ models/index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-const fs        = require('fs');
-const path      = require('path');
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
-const basename  = path.basename(module.filename);
-const env       = process.env.NODE_ENV || 'development';
-const config    = require(__dirname + '/../config/config.json')[env];
-const db        = {};
+const basename = path.basename(module.filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.js')[env];
+const db = {};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
