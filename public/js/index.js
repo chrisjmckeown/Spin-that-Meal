96b4e16657638d2d0d2a12457486a2b872ceb84c
Avvi Singh <singh.avvi98@gmail.com>
2020-11-16 21:21:19 +1100

    Update to header drop-down button

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,9 @@
 $(document).ready(() => {
     const userDetails = JSON.parse(localStorage.getItem("user-details"));
 
+    const yourProfile = $('#your-profile');
+    yourProfile.html('<div class="user-icon"><i class="fa fa-user-circle"></i></div>' + userDetails.userName);
+
     const username = $("#username")
     username.text(userDetails.userName);
 
