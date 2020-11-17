8c7e2e84c54c8a4f3300c4d68be548c23f2286c6
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-11-17 21:19:40 +1100

    login forms placement fixed

--- public/js/signup.js
+++ public/js/signup.js
@@ -12,6 +12,8 @@ $(document).ready(() => {
     // When the signup button is clicked, we validate the email and password are not blank
     signUpForm.on("submit", event => {
         event.preventDefault();
+        $("#alert .msg").text("1");
+        $("#alert").fadeOut(0);
         const userData = {
             firstName: firstNameInput.val().trim(),
             lastName: lastNameInput.val().trim(),
@@ -21,10 +23,30 @@ $(document).ready(() => {
             phone: phoneInput.val().trim(),
             address: addressInput.val().trim()
         };
+        if (userData.password.length < 8) {
+            $("#alert .msg").text("Password must be greater than 8 characters");
+            $("#alert").fadeIn(500);
+            return;
+        }
+        // // list of characters, will control upper and lower with lowerCase and upperCase variables
+        // const characterList = "abcdefghijklmnopqrstuvwyxz";
+
+        // // list for special characters !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+        // const specialCharactersCaseList = "\u0020!\u0022#$%&'()*+,-./:;<=>?@[\u005C\u005D^_`{|}~";
+        // if (!specialCharactersCaseList.some(v => userData.password.includes(v))) {
+        //     $("#alert .msg").text("Password must contain 1 special character");
+        //     $("#alert").fadeIn(500);
+        //     return;
+        // }
+        // // list of numbers characters
+        // const numberList = "0123456789";
 
         if (!userData.firstName || !userData.lastName || !userData.userName
             || !userData.email || !userData.password
             || !userData.phone || !userData.address) {
+
+            $("#alert .msg").text("Please ensure all details are correctly entered.");
+            $("#alert").fadeIn(500);
             return;
         }
         // If we have an email and password, run the signUpUser function
--- public/js/toggle-login-page.js
+++ public/js/toggle-login-page.js
@@ -1,29 +1,46 @@
 $(document).ready(() => {
     // Getting references to our form and inputs
     const signUpForm = $("form.sign-up-form");
-    const signIpForm = $("form.sign-ip-form");
+    const signInForm = $("form.sign-ip-form");
 
     const signIn = $(".sign-in");
     const signInSwitch = $(".sign-in-switch");
     const signUp = $(".sign-up");
     const signUpSwitch = $(".sign-up-switch");
+    const moveme = $(".moveme");
 
     signInSwitch.hide();
     signUp.hide();
 
     signUpForm.on("submit", event => {
         event.preventDefault();
-        signIn.hide();
-        signUpSwitch.hide();
         signInSwitch.show();
         signUp.show();
+        // if (signUpSwitch.hasClass("fadeout"))
+        //     signUpSwitch.removeClass("fadeout").addClass("fadein");
+        // else
+        //     signUpSwitch.removeClass("fadein").addClass("fadeout");
+        // if (signIn.hasClass("fadeout"))
+        //     signIn.removeClass("fadeout").addClass("fadein");
+        // else
+        //     signIn.removeClass("fadein").addClass("fadeout");
+        signIn.hide();
+        signUpSwitch.hide();
     });
 
-    signIpForm.on("submit", event => {
+    signInForm.on("submit", event => {
         event.preventDefault();
-        signIn.show();
-        signUpSwitch.show();
         signInSwitch.hide();
         signUp.hide();
+        // if (signUpSwitch.hasClass("fadeout"))
+        //     signUpSwitch.removeClass("fadeout").addClass("fadein");
+        // else
+        //     signUpSwitch.removeClass("fadein").addClass("fadeout");
+        // if (signIn.hasClass("fadeout"))
+        //     signIn.removeClass("fadeout").addClass("fadein");
+        // else
+        //     signIn.removeClass("fadein").addClass("fadeout");
+        signIn.show();
+        signUpSwitch.show();
     });
 });
