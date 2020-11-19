05a4012b8384e4437594a3bbd520d5de1be21977
Lionhatt <gavindxy@gmail.com>
2020-11-19 19:11:44 +1100

    pull

---
+++ public/js/create_recipe.js
@@ -0,0 +1,13 @@
+$(function() {
+    const recipeName = $("#recipeName");
+    const recipeServing = $("#recipeServing");
+    const recipeInstruction = $("#recipeInstruction");
+    const saveRecipe = $("#saveRecipe");
+    const { id } = JSON.parse(localStorage.getItem("user-details"));
+
+    
+
+
+
+
+})
